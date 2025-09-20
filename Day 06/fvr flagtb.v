afdaa8e16282f65a4460087cb11c40bd00d1ef84
ANBU <anbubalu004@gmail.com>
2025-09-20 22:39:50 +0530

    Create fvr flagtb.v

---
+++ Day 06/fvr flagtb.v
@@ -0,0 +1,13 @@
+module forever_flagtb;
+  wire clk;
+  forever_flag uut (.clk(clk));
+
+  initial begin
+    $dumpfile("forever_flag.vcd");   
+    $dumpvars(0, tb_forever_flag);
+    $monitor("Time=%0t | clk=%b", $time, clk);
+    #100; 
+    $finish;
+  end
+
+endmodule
