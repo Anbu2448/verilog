edc2a4d3570040833df6489f491c67194ad7dea0
ANBU <anbubalu004@gmail.com>
2025-10-08 17:29:50 +0530

    Update generate_clocktb.v

--- Day 05/generate_clocktb.v
+++ Day 05/generate_clocktb.v
@@ -1,10 +1,8 @@
 `timescale 1ns / 1ps
-
 module generate_clocktb;
     reg clk_50MHz;
     reg reset_n;
     wire clk_100Hz;
-
 generate_clock uut (
         .clk_50MHz(clk_50MHz),
         .reset_n(reset_n),
@@ -13,8 +11,7 @@ generate_clock uut (
 initial begin
      clk_50MHz = 0;
       forever #10 clk_50MHz = ~clk_50MHz; 
-  end
-
+end
     initial begin
        $monitor("Time = %0t | clk_50MHz = %b | reset_n = %b | clk_100Hz = %b", 
                   $time, clk_50MHz, reset_n, clk_100Hz);
