ff590ba6ca2e5f6a8b80e8cf78c18a09e787a6d2
ANBU <anbubalu004@gmail.com>
2025-10-08 17:30:44 +0530

    Update generate_clock.v

--- Day 05/generate_clock.v
+++ Day 05/generate_clock.v
@@ -1,5 +1,4 @@
 `timescale 1ns / 1ps
-
 module generate_clock (
     input clk_50MHz,
     input reset_n,
@@ -7,17 +6,16 @@ module generate_clock (
 );
     parameter integer div_count = 250_000;
     reg [18:0] counter;  
-
     always @(posedge clk_50MHz or negedge reset_n) begin
         if (!reset_n) begin
-            counter   <= 0;
-            clk_100Hz <= 0;
+         counter   <= 0;
+         clk_100Hz <= 0;
         end else begin
-            if (counter == div_count-1) begin
-                counter   <= 0;
-                clk_100Hz <= ~clk_100Hz; 
-            end else begin
-                counter <= counter + 1;
+         if (counter == div_count-1) begin
+            counter   <= 0;
+             clk_100Hz <= ~clk_100Hz; 
+        end else begin
+               counter <= counter + 1;
             end
         end
     end
